3c32aa0c3af30153588f1f35198bd6b0621c44bb
TshwaneloMatlou <tshwanelomatlou55@gmail.com>
2023-08-01 03:45:02 -0700

    0x05. AirBnB clone - RESTful API

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -6,31 +6,19 @@ from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
-
-@app_views.route('/status', strict_slashes=False)
-def status():
-    """Return status of application
+@app_views.route('/api/v1/stats', methods=['GET'])
+def get_stats():
     """
-    return jsonify({'status': 'OK'})
-
-
-@app_views.route('/stats', strict_slashes=False)
-def stats():
-    """Retrieve count of objects in storage
+    Retrieves the number of each object by type.
     """
-    from models.amenity import Amenity
-    from models.city import City
-    from models.place import Place
-    from models.review import Review
-    from models.state import State
-    from models.user import User
+    stats = {
+        'amenities': storage.count('Amenity'),
+        'cities': storage.count('City'),
+        'places': storage.count('Place'),
+        'reviews': storage.count('Review'),
+        'states': storage.count('State'),
+        'users': storage.count('User'),
+        # Add more object types as needed
+    }
 
-    classes = {"amenities": Amenity, "cities": City,
-               "places": Place, "reviews": Review,
-               "states": State, "users": User}
-    json_dict = {}
-
-    for name, cls in classes.items():
-        json_dict.update({name: storage.count(cls)})
-
-    return jsonify(json_dict)
+    return jsonify(stats)
